1446154f14e4d293974645e301389c30526ccb86
Sa1ka <760133018@qq.com>
2022-11-30 19:01:31 +0800

    登陆注册完善

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="wrapper">
-    <div style="margin: 100px auto; background-color: #fff; width: 350px; height: 400px; padding: 20px; border-radius: 10px">
-      <div style="margin: 20px 0; text-align: center; font-size: 24px"><b>登 录</b></div>
+    <div style="margin: 100px auto; background-color: #fff; width: 350px; height: 420px; padding: 20px; border-radius: 10px">
+      <div style="margin: 20px 0; text-align: center; font-size: 24px"><b>大学生实习管理系统</b></div>
+      <div style="margin: 20px 0; text-align: center; font-size: 22px"><b>登 录</b></div>
       <el-form :model="user" :rules="rules" ref="userForm">
         <el-form-item prop="role">
           <el-select v-model="user.role" placeholder="请选择登录身份" size="medium" style="margin: 10px 0;width: 100%">
@@ -18,7 +19,7 @@
           <el-input placeholder="请输入密码" size="medium" style="margin: 10px 0" prefix-icon="el-icon-lock" show-password
                     v-model="user.password" @keyup.enter.native="login"></el-input>
         </el-form-item>
-        <el-form-item style="margin: 10px 0; text-align: right">
+        <el-form-item style="margin: 10px 0; text-align: right;">
           <el-button type="warning" size="small" autocomplete="off" @click="$router.push('/register')">注册</el-button>
           <el-button type="primary" size="small" autocomplete="off" @click="login">登录</el-button>
         </el-form-item>
@@ -107,8 +108,9 @@ export default {
 
 <style>
 .wrapper {
-  height: 100vh;
+  height: 100%;
   background-image: linear-gradient(to bottom right, #FC16F4, #39C5BB);
   overflow: hidden;
+  padding: 20px;
 }
 </style>
